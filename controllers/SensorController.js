29442bf51bbb0588c0cff5c2c6604f6e01b7142e
Adam Firdaus <adam.firdauz22@gmail.com>
2019-05-24 12:37:51 +0700

    tambah status sensor

--- controllers/SensorController.js
+++ controllers/SensorController.js
@@ -4,7 +4,10 @@ class SensorController {
     static async get(req,res,next){
         let data = await axios('http://156.67.217.195:2000/data_realtime');
         console.log(data.data);
-        return res.json({data:data.data});
+        if(data.data)
+            return res.json({status:"200",data:data.data});
+        else
+            return res.json({status:"500",message:"Terjadi kesalahan"});
     }
 }
 
